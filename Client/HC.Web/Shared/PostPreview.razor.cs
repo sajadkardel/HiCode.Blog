f3da0b03e9913cb30c3bcbd1b9c11405c673871b
skardel <s.kardel@sls.ir>
2023-07-17 14:30:51 -0700

    Redesign UI

---
+++ Client/HC.Web/Shared/PostPreview.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace HC.Web.Shared;
+
+public partial class PostPreview
+{
+    [Parameter] public string Title { get; set; } = string.Empty;
+    [Parameter] public string ShortDescription { get; set; } = string.Empty;
+    [Parameter] public string ImageSrc { get; set; } = "/images/icons/icon-512.svg";
+    [Parameter] public string ImageAlt { get; set; } = "HiCode";
+
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        Title = "نکاتی درمورد Static Constructor در سیشارپ";
+        ShortDescription = "یکی از سوالاتی که در مصاحبه ها فنی پرسیده میشه موارد مرتبط به Static Constructor هست که حتی اگر تا به حال از اون استفاده نکردید باید درموردش اطلاعاتی داشتته باشید. اول باید بدونیم اصلا چه نیازی به این قابلیت سیشارپ خواهیم داشت. در شرایطی ممکن است یک ممبر Static درون کلاس داشه باشیم که مقدار این ممبر Static از قبل برای ما یک مقدار ثابت و مشخص نیست. یعنی نمیتوانیم آن را به صورت";
+    }
+}
