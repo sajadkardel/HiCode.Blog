55bab14a1711f03b36d0e8cadc3853ac7027775f
sajad kardel <sajadkardel@outlook.com>
2023-09-29 02:34:55 +0330

    Merge Conflicts

--- Client/HC.Web/Shared/NavMenu.razor.cs
+++ Client/HC.Web/Shared/NavMenu.razor.cs
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Routing;
-using Microsoft.JSInterop;
-=======
-﻿using HC.Shared.Dtos.Blog;
-using HC.Shared.Services.Contracts;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.Extensions.Logging;
->>>>>>> d19f618a4461ed9ca1f7a03b5263c4377b1828ae
+using Microsoft.AspNetCore.Components.Routing;
 
 namespace HC.Web.Shared;
 
 public partial class NavMenu : AppBaseComponent, IDisposable
 {
-<<<<<<< HEAD
     [Inject] public NavigationManager Navigation { get; set; } = default!;
 
     [Parameter] public bool IsOpenNav { get; set; }
@@ -33,13 +23,5 @@ public partial class NavMenu : AppBaseComponent, IDisposable
     public void Dispose()
     {
         Navigation.LocationChanged -= HandleLocationChanged;
-=======
-    [Parameter] public bool IsOpenNav { get; set; }
-    [Parameter] public EventCallback OnToggleNav { get; set; }
-
-    private async Task ToggleNav()
-    {
-        //await OnToggleNav.InvokeAsync();
->>>>>>> d19f618a4461ed9ca1f7a03b5263c4377b1828ae
     }
 }
